964f62f92be522f0882bf0d168e98c2b010ad1eb
= <=>
2022-10-06 21:08:39 +0100

    Edited readme file

--- monty_helper_functions.c
+++ monty_helper_functions.c
@@ -18,7 +18,7 @@ int read_execute_file(char *fileName, stack_t **stack)
 
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", fileName);
+		fprintf(stderr, "Error: Can't open file %s\n", fileName);
 		exit(EXIT_FAILURE);
 	}
 	while ((size_read = getline(&line, &buff, file)) != -1)
